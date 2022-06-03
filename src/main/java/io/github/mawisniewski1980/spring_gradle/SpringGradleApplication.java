5cf69e8580cc0de33c01617a6ff385ec5274695e
WISNIA <mawisniewski1980@gmail.com>
2022-06-03 21:43:42 +0200

    init

---
+++ src/main/java/io/github/mawisniewski1980/spring_gradle/SpringGradleApplication.java
@@ -0,0 +1,13 @@
+package io.github.mawisniewski1980.spring_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mawisniewski1980/spring_gradle/SpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mawisniewski1980.spring_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
